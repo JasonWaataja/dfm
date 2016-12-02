bcbfd6dbe9053c11797483f2904c19e71c3b1cf9
Jason Waataja <jasonswaataja@gmail.com>
2016-12-01 22:48:11 -0800

    Got argument parser to work I think

--- src/dfm.lisp
+++ src/dfm.lisp
@@ -6,37 +6,34 @@
   ((start-directory
     :initarg :start-directory
     :initform (make-pathname :defaults (user-homedir-pathname))
-    :accessor start-directory))
+    :accessor start-directory)
   (config-file
    :initarg :config-file
    :initform nil
-   :accessor config-file))
+   :accessor config-file)))
 
 (defparameter *getopt-args* '(("directory" :required)
 			      ("config-file" :required)))
 
 (defun parse-argv (argv)
-  (loop
-     with current-argument-list = (copy-list argv)
-     with matched-arguments = nil
-     with unmatched-required = nil
-     with options = (make-instance 'dfm-options)
-     do
-       (setf current-argument-list
-	     (getopt:getopt current-argument-list *getopt-args*))
-     if (not (null unmatched-required))
-     do
-       ;; TODO: Add an error condition.
-       (format t "Unmatched arguments: ~a~%" unmatched-required)
-       (return nil)
-     else
-     if (null matched-arguments) return options
-     end
-     else do
-       
-       
+  (let ((options (make-instance 'dfm-options)))
+    (multiple-value-bind (filtered-argument-list
+			  matched-options
+			  unmatched-required)
+	(getopt:getopt argv *getopt-args*)
+      (cond ((not (null unmatched-required))
+	     ;; TODO: Add an error condition.
+	     (format t "Failed to provide required arguments: ~a~%" unmatched-required)
+	     (values nil nil))
+	    (t (let ((directory-cons (assoc "directory" matched-options :test #'string=))
+		     (config-file-cons (assoc "config-file" matched-options :test #'string=)))
+		 (when directory-cons
+		   (setf (slot-value options 'start-directory) (cdr directory-cons)))
+		 (when config-file-cons
+		   (setf (slot-value options 'config-file) (cdr config-file-cons)))
+		 (values options filtered-argument-list)))))))
 
 (defun main (argv)
   "Main function for dfm"
-  (format t "~a~%" argv))
+  (parse-argv argv))
 
